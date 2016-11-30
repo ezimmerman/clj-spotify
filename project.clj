6ce6cede5b10089019d4019e2f2066447f6c4c01
Eric Zimmerman <eric.zimmerman@infor.com>
2016-11-30 15:37:30 -0600

    clean up

--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
                  [http-kit "2.1.18"]
                  [com.stuartsierra/component "0.3.1"]
                  [org.clojure/core.async "0.2.391"]
-                 [environ "1.1.0"]
                  [http-kit.fake "0.2.1"]
                  [cheshire "5.6.3"]
                  [levand/immuconf "0.1.0"]]
-  :plugins [[lein-expectations "0.0.7"]
-            [environ "1.1.0"]]
+  :plugins []
   :main ^:skip-aot clj-spotify.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
